cd1c3c613b628596de6550327c774796f8762ae5
Amir Hossein Goodarzi (Numb) <amir007ag@gmail.com>
2017-02-05 18:56:07 +0330

    add some test. :)
    Signed-off-by: Amir Hossein Goodarzi (Numb) <amir007ag@gmail.com>

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,16 @@
 import unittest
 
+from pydigikala import Incredibleoffer
 
-class MyTestCase( unittest.TestCase ):
-    def test_something(self):
-        self.assertEqual( True, False )
+
+class IncridibleTest(unittest.TestCase):
+    def testPass(self):
+        return
+
+
+def getresponseincredibleoffer(self):
+    self.assertEqual(str(Incredibleoffer()), "<Response [200]>")
 
 
 if __name__ == '__main__':
-    unittest.main( )
+    unittest.main()
